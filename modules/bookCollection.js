e7b9e3766ca04c5145ee4334e4e4fbaf3625b2ca
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-28 13:44:03 +0200

    Use of arrow functions and update id

--- modules/bookCollection.js
+++ modules/bookCollection.js
@@ -1,54 +1,46 @@
-class BookCollection {
-  constructor(bookArray) {
-    this.bookArray = bookArray;
-  }
+let bookArray = [];
 
-  displayBooks() {
-    const bookContainer = document.querySelector('.books-container');
-    bookContainer.innerHTML = '';
-    this.bookArray.forEach((book) => {
-      bookContainer.insertAdjacentHTML(
-        'beforeend',
-        `
+const displayBooks = (book) => {
+  const bookContainer = document.querySelector(".books-container");
+  bookContainer.innerHTML = "";
+  bookArray.forEach((book) => {
+    bookContainer.insertAdjacentHTML(
+      "beforeend",
+      `
         <div class="book-details" id="book-${book.id}">
           <p>"${book.title}" by ${book.author}</p> 
           <button class="remove" type="button" id="remove-${book.id}">Remove</button>
-        </div>`,
-      );
+        </div>`
+    );
+  });
+
+  if (bookArray.length !== 0) {
+    bookContainer.style.border = "solid 3px #000000";
+  } else bookContainer.style.border = "none";
+};
+
+const storeInLocalBrowser = () => {
+  localStorage.setItem("bookCollectionArray", JSON.stringify(bookArray));
+};
+
+const removeBook = (book) => {
+  bookArray.splice(book, 1);
+  storeInLocalBrowser();
+  displayBooks();
+};
+const addBook = (titleInput, authorInput) => {
+  if (titleInput.value && authorInput.value) {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    bookArray.push({
+      title,
+      author,
+      id: new Date.now(),
     });
-
-    if (this.bookArray.length !== 0) {
-      bookContainer.style.border = 'solid 3px #000000';
-    } else bookContainer.style.border = 'none';
+    titleInput.value = "";
+    authorInput.value = "";
+    storeInLocalBrowser();
+    displayBooks();
   }
-
-  storeInLocalBrowser() {
-    localStorage.setItem('bookCollectionArray', JSON.stringify(this.bookArray));
-  }
-
-  removeBook(book) {
-    this.bookArray.splice(book, 1);
-    this.storeInLocalBrowser();
-    this.bookArray.forEach((book, i) => {
-      book.id = i;
-    });
-    this.displayBooks();
-  }
-
-  addBook(titleInput, authorInput) {
-    if (titleInput.value && authorInput.value) {
-      const title = titleInput.value;
-      const author = authorInput.value;
-      this.bookArray.push({
-        title,
-        author,
-        id: this.bookArray.length,
-      });
-      titleInput.value = authorInput.value = '';
-      this.storeInLocalBrowser();
-      this.displayBooks();
-    }
-  }
-}
-
-export { BookCollection };
+};
+export { addBook, removeBook };
