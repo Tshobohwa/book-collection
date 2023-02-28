05e9c04d01f807da920399ca2644241061b09ff2
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-28 10:46:33 +0200

    update script source

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { DateTime } from './modules/luxon.js';
-
 import {
   addButton,
   titleInput,
@@ -8,42 +6,44 @@ import {
   navBar,
   bookContainer,
   DateAndTime,
-} from './modules/dom.js';
+} from "./dist/dom.js";
 
-import { BookCollection } from './modules/bookCollection.js';
+import { BookCollection } from "./dist/bookCollection.js";
+
+const { DateTime } = require("luxon");
 
 const bookCollection = new BookCollection([]);
 addButton.addEventListener(
-  'click',
-  bookCollection.addBook.bind(bookCollection, titleInput, authorInput),
+  "click",
+  bookCollection.addBook.bind(bookCollection, titleInput, authorInput)
 );
 
-navBar.addEventListener('click', (e) => {
-  if (e.target.classList.contains('nav-link')) {
-    const anchorHref = e.target.href.split('#')[1];
-    document.querySelectorAll('.section').forEach((section) => {
-      if (section.id === anchorHref) section.style.display = 'flex';
-      else section.style.display = 'none';
+navBar.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav-link")) {
+    const anchorHref = e.target.href.split("#")[1];
+    document.querySelectorAll(".section").forEach((section) => {
+      if (section.id === anchorHref) section.style.display = "flex";
+      else section.style.display = "none";
     });
   }
 });
 
 sections.forEach((section) => {
-  if (section === sections[1]) section.style.display = 'flex';
-  else section.style.display = 'none';
+  if (section === sections[1]) section.style.display = "flex";
+  else section.style.display = "none";
 });
 
-bookContainer.addEventListener('click', (e) => {
-  if (e.target.classList.contains('remove')) {
+bookContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
     const removeBtn = e.target;
     const book = bookCollection.bookArray.find(
-      (book) => book.id === removeBtn.id.split('-')[1],
+      (book) => book.id === removeBtn.id.split("-")[1]
     );
     bookCollection.removeBook(book);
   }
 });
 
-const getCurrentTime = function () {
+const getCurrentTime = () => {
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
   DateAndTime.textContent = now;
 };
