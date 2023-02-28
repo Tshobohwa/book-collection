84896f3083aff4fc0d6a11e6feae4df9c6fb6228
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-28 10:45:15 +0200

    Update modules

--- modules/dom.js
+++ modules/dom.js
@@ -1,11 +1,11 @@
-const addButton = document.querySelector('.add-button');
-const titleInput = document.querySelector('#title-input');
-const authorInput = document.querySelector('#author-input');
+const addButton = document.querySelector(".add-button");
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
 
-const sections = document.querySelectorAll('.section');
-const navBar = document.querySelector('.nav-Bar');
-const bookContainer = document.querySelector('.books-container');
-const DateAndTime = document.querySelector('.date-and-time');
+const sections = document.querySelectorAll(".section");
+const navBar = document.querySelector(".nav-Bar");
+const bookContainer = document.querySelector(".books-container");
+const DateAndTime = document.querySelector(".date-and-time");
 
 export {
   addButton,
